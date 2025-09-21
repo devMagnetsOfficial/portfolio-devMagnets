d9dca300de155a98ff5becd94c915f5cf4dcd2eb
abhishek <singhabhishek.engineer@gmail.com>
2025-09-21 14:07:47 +0530

    footer is added which is left to commit in previous footer commit  message

---
+++ frontend/src/component/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return (<>
+    <div className="border-2 w-full h-10 bg-darkGray text-textSecondary text-xs flex justify-between items-center px-4">
+        <span>&copy; 2025 All Right Reserved</span>
+        <span>Email : devmagnets.mail@gmail.com</span>
+    </div>
+    </>)
+}
