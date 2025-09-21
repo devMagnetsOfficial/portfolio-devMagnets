446acf599b54852099b9f86a5d319b48d0f07af5
abhishek <singhabhishek.engineer@gmail.com>
2025-09-21 22:13:29 +0530

    layout is done with left right menu and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
-import LeftMenu from "./component/LeftMenu"
 import RightMenu from "./component/RightMenu"
-import Footer from "./component/Footer"
+import Home from "./pages/Home/Home"
+import PublicLayout from "./layout/PublicLayout"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 export default function App() {
   return (<>
-  <Footer/>
-    {/* <BrowserRouter>
+
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<RightMenu />} />
-        <Route path='/portfolio' element={<RightMenu />} />
-        <Route path='history' element={<RightMenu />} />
-        <Route path='/blog' element={<RightMenu />} />
-        <Route path='/contact' element={<RightMenu />} />
-        <Route path='/donate' element={<RightMenu />} />
-        <Route path='/mentorship' element={<RightMenu />} />
-        <Route path='*' element={<><div>page is under construction</div></>} />
+        <Route element={<PublicLayout/>}>
+          <Route path='/' element={<Home />} />
+          <Route path='/portfolio' element={<RightMenu />} />
+          <Route path='history' element={<RightMenu />} />
+          <Route path='/blog' element={<RightMenu />} />
+          <Route path='/contact' element={<RightMenu />} />
+          <Route path='/donate' element={<RightMenu />} />
+          <Route path='/mentorship' element={<RightMenu />} />
+          <Route path='*' element={<><div>page is under construction</div></>} />
+        </Route>
       </Routes>
-    </BrowserRouter> */}
+    </BrowserRouter>
   </>)
 
 }
--- frontend/src/component/RightMenu.jsx
+++ frontend/src/component/RightMenu.jsx
@@ -8,20 +8,23 @@ export default function RightMenu() {
     const setMenubar = () => {
         setMenubarClicked(!isMenubarClicked)
     }
+
     return (<>
-        {/* <LeftMenu/> */}
-        <div className={`bg-darkGray h-[100vh] ${isMenubarClicked ? 'w-[250px]' : 'w-[70px]'} transition-all duration-1000 ease-in-out absolute right-0 `}>
-            <div className={`bg-dark flex ${isMenubarClicked ? 'pl-4' : 'justify-center'} h-[100px] items-center`}>
+
+        <div className={`bg-darkGray   ${isMenubarClicked ? 'w-[250px] h-[100vh]' : 'w-[50px] h-[0px] '} transition-width duration-1000 ease-in-out absolute    right-0`}>
+            <div className={`bg-dark flex ${isMenubarClicked ? 'pl-4' : 'justify-center'} h-[70px] items-center`}>
                 {isMenubarClicked ?
-                    <MdClose className="text-2xl text-textSecondary" onClick={setMenubar} />
+                    <MdClose className="md:text-2xl text-textSecondary" onClick={setMenubar} />
                     :
-                    <MdMenu className="text-2xl text-textSecondary" onClick={setMenubar} />
+                    <MdMenu className="md:text-2xl text-textSecondary" onClick={setMenubar} />
                 }
 
             </div>
 
-            <div className="flex justify-center  items-center  text-textSecondary uppercase  min-h-[100px]">
-                {isMenubarClicked ?
+            <div className="flex justify-center   items-center  text-textSecondary uppercase  min-h-[100px]">
+               {!isMenubarClicked&& <div className={` rotate-90 md:block  hidden`}>home</div>}
+
+                {isMenubarClicked &&
                     <>
                         <div className="flex flex-col w-full">
                             <div className="mt-[100px]  w-full p-5 text-sm ">
@@ -38,15 +41,11 @@ export default function RightMenu() {
                                 <ul className="flex flex-col gap-2 ">
                                     <NavLink to='/donate' className={({ isActive }) => (isActive ? 'text-white font-semibold' : '')}>donate</NavLink>
                                     <NavLink to='/mentorship' className={({ isActive }) => (isActive ? 'text-[white]  font-semibold' : '')}>MentorShip</NavLink>
-                                   </ul>
+                                </ul>
                             </div>
-
-
-                        </div>  </>
-                    :
-                    <>
-                        <span className={`origin-center transition-transform duration-1000 ease-in-out ${isMenubarClicked?'rotate-0':'rotate-90'}`}>home</span>
-                    </>}
+                        </div>
+                    </>
+                }
             </div>
 
 
