789e50e8d24454a906cb1d693c9322dbf71ae9d1
abhishek <singhabhishek.engineer@gmail.com>
2025-09-26 02:22:02 +0530

    client can contact me

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,44 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+require('dotenv').config({})
 app.use(express.json())
+app.use(cors())
 const nodemailer = require('nodemailer')
 app.get('/', (req, res) => {
     res.send('server is running buddy ')
 })
 app.post('/sendMail', (req, res) => {
-    const {name,mail,message}=req.body
+    console.log('req.body:', req.body);
+    const { name, mail, message } = req.body
     const transporter = nodemailer.createTransport(
         {
             service: 'gmail',
             auth: {
-                user: 'devmagnets.mail@gmail.com',
-                pass: 'apze dmjo lapv iulx'
+                user: process.env.SENDERGMAIL,
+                pass: process.env.APPPASSWORD
             }
         }
     )
-    const mailOption={
-        from:'devmagnets.mail@gmail.com',
-        to:'singhabhishek.engineer@gmail.com',
-        subject:'Enquiry!',
-        text:`hi im ${name} , ${message}`,
-        replyTo:mail
+    const mailOption = {
+        from: process.env.SENDERGMAIL,
+        to: process.env.ADMINGMAIL,
+        subject: 'Enquiry!',
+        text: `hi im ${name} , ${message}`,
+        replyTo: mail
     }
-    transporter.sendMail(mailOption,(err,info)=>{
-        if(err){
-            console.log("error",err)
+    transporter.sendMail(mailOption, (err, info) => {
+        if (err) {
+            console.log("error", err)
+            res.json({ success: false })
         }
-        else{
-            console.log('sent',info.response)
+        else {
+            console.log('sent', info.response)
+            res.json({ success: true })
         }
+
     })
+
 
 })
 app.listen(2030, () => {
