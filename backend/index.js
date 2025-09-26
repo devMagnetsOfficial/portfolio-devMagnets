53e0624ac5cce543b375328228180148c5a7a8fc
abhishek <singhabhishek.engineer@gmail.com>
2025-09-26 23:22:36 +0530

    mail is working

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+
 const app = express()
+const ejs=require('ejs')
 require('dotenv').config({})
 app.use(express.json())
 app.use(cors())
@@ -8,36 +10,39 @@ const nodemailer = require('nodemailer')
 app.get('/', (req, res) => {
     res.send('server is running buddy ')
 })
-app.post('/sendMail', (req, res) => {
+app.post('/sendMail', async (req, res) => {
     console.log('req.body:', req.body);
     const { name, mail, message } = req.body
-    const transporter = nodemailer.createTransport(
-        {
-            service: 'gmail',
-            auth: {
-                user: process.env.SENDERGMAIL,
-                pass: process.env.APPPASSWORD
+    try {
+        const transporter = nodemailer.createTransport(
+            {
+                service: 'gmail',
+                auth: {
+                    user: process.env.SENDERGMAIL,
+                    pass: process.env.APPPASSWORD
+                }
             }
+        )
+        const mailOption = {
+            from: process.env.SENDERGMAIL,
+            to: process.env.ADMINGMAIL,
+            subject: 'Enquiry!',
+            text: `hi im ${name} , ${message}`,
+            replyTo: mail
         }
-    )
-    const mailOption = {
-        from: process.env.SENDERGMAIL,
-        to: process.env.ADMINGMAIL,
-        subject: 'Enquiry!',
-        text: `hi im ${name} , ${message}`,
-        replyTo: mail
+        const responseMail=await ejs.renderFile('./mail/responseMailTemplate.ejs',{name,message})
+        const responseMailOption = {
+            from: process.env.ADMINGMAIL,
+            to: mail,
+            subject: 'Response to Your Email',
+            html: responseMail
+        }
+        await transporter.sendMail(mailOption)
+        await transporter.sendMail(responseMailOption)
+        res.json({ success: true })
+    } catch (err) {
+        res.json({ success: false })
     }
-    transporter.sendMail(mailOption, (err, info) => {
-        if (err) {
-            console.log("error", err)
-            res.json({ success: false })
-        }
-        else {
-            console.log('sent', info.response)
-            res.json({ success: true })
-        }
-
-    })
 
 
 })
