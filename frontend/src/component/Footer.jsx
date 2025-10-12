edeb87d198cc380c5bca51e4431689fef44491ca
abhishek <singhabhishek.engineer@gmail.com>
2025-10-12 17:46:14 +0530

    footer mail working

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -2,8 +2,7 @@ export default function Footer(){
     return (<>
     <div className="  h-10 bg-card px-5 text-textSecondary text-[10px] lg:xs flex justify-between items-center ">
         <span>&copy; 2025 All Right Reserved</span>
-
-        <span>Email : devmagnets.mail@gmail.com</span>
+        <a href="mailto:devmagnets.mail@gmail.com">Email : devmagnets.mail@gmail.com</a>
     </div>
     </>)
 }
