2da28c6a5caa32302fbdc547a3b5ec8a5c980b5c
abhishek <singhabhishek.engineer@gmail.com>
2025-10-12 12:30:53 +0530

    slider is working in all screen

--- frontend/src/pages/home/Recommendation.jsx
+++ frontend/src/pages/home/Recommendation.jsx
@@ -5,7 +5,15 @@ import Recommendations from "./recommendData";
 
 
 export default function Recommendation() {
-// on changing screensize show 1 and 2 template respescitvelty 
+
+const [screenWidth,setScreenWidth]=useState(window.innerWidth)
+const settingscreenWidth=()=>{
+    return setScreenWidth(window.innerWidth)
+}
+useEffect(()=>{
+    window.addEventListener('resize',settingscreenWidth)
+
+},[])
     const totalReview = (Recommendations.length)
     const [sidx, setIdx] = useState(1);
     const star = (rate, element) => {
@@ -23,9 +31,12 @@ export default function Recommendation() {
         setIdx((sidx - 1 + totalReview) % totalReview)
         console.log(sidx)
     }
+    const customSlide=(idx)=>{
+        setIdx(idx)
+    }
     const isvisible = (idx) => {
         return (sidx === idx ||
-            ((window.innerWidth >= 1024) && sidx === (idx + 1) % totalReview)
+            ((screenWidth >= 768) && sidx === (idx + 1) % totalReview)
         )
     }
 
@@ -40,7 +51,7 @@ export default function Recommendation() {
                 {
                     Recommendations.map((items, idx) => (
                         isvisible(idx) && (
-                            <div key={idx} className=" flex w-[calc(100%-30px)] lg:w-[calc(50%-30px)]  flex-shrink-0  flex-col gap-4 p-8 text-sm bg-white/10 min-h-[70%]  relative ">
+                            <div key={idx} className=" flex w-[100%]  md:w-[50%] lg:w-[50%]  flex-shrink-0  flex-col gap-4 p-8 text-sm bg-white/10 min-h-[70%]  relative ">
                                 <div className="flex justify-between items-center">
                                     <div className="text-textPrimary">
                                         <h1 className="font-semibold " >{items.name}</h1>
@@ -67,7 +78,7 @@ export default function Recommendation() {
             <div className=" text-textPrimary flex justify-between mb-5">
                 <div className="flex gap-2">
                     {Recommendations.map((element, idx) => (
-                        <span key={idx} className={`cursor-pointer transition-all duration-300 ease-in-out  w-5 rounded-xl h-1  font-bold ${isvisible(idx) ? 'bg-accent' : 'bg-gray-300'}`}>
+                        <span key={idx} onClick={()=>customSlide(idx)} className={`cursor-pointer transition-all duration-300 ease-in-out  w-5 rounded-xl h-1  font-bold ${isvisible(idx) ? 'bg-accent' : 'bg-gray-300'}`}>
                         </span>
                     ))}
                 </div>
