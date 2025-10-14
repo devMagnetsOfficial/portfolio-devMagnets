04524d017b6254a8345beb24ae9b767c08f760b2
abhishek <singhabhishek.engineer@gmail.com>
2025-10-14 22:00:29 +0530

    backend code starts

--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,31 @@
-const express = require('express')
-const cors = require('cors')
-
-const app = express()
-const ejs=require('ejs')
-app.set('view engine','ejs')
-app.set('views','./mail')
-require('dotenv').config({})
-app.use(express.json())
-app.use(cors())
-const nodemailer = require('nodemailer')
-app.get('/', (req, res) => {
-   res.render('responseMailTemplate.ejs',{name:'abhishek',message:'i want your service'})
-})
-app.post('/sendMail', async (req, res) => {
-    console.log('req.body:', req.body);
-    const { name, mail, message } = req.body
+// env 
+require('dotenv').config({ path: '' })
+const PORT = process.env.PORT
+// db
+const mongoose = require('mongoose')
+const MONGODATABASE_LINK = process.env.MONGODATABASE_LINK
+console.log(MONGODATABASE_LINK)
+const db = async () => {
     try {
-        const transporter = nodemailer.createTransport(
-            {
-                service: 'gmail',
-                auth: {
-                    user: process.env.SENDERGMAIL,
-                    pass: process.env.APPPASSWORD
-                }
-            }
-        )
-        const mailOption = {
-            from: process.env.SENDERGMAIL,
-            to: process.env.ADMINGMAIL,
-            subject: 'Enquiry!',
-            text: `hi im ${name} , ${message}`,
-            replyTo: mail
-        }
-        const responseMail=await ejs.renderFile('./mail/responseMailTemplate.ejs',{name,message})
-        const responseMailOption = {
-            from: process.env.ADMINGMAIL,
-            to: mail,
-            subject: 'Response to Your Email',
-            html: responseMail
-        }
-        await transporter.sendMail(mailOption)
-        await transporter.sendMail(responseMailOption)
-        res.json({ success: true })
-    } catch (err) {
-        res.json({ success: false })
+        await mongoose.connect(MONGODATABASE_LINK)
+        console.log('DB Connected Sucessfully')
+    }catch(err){
+        console.log('DB Connection Error'+err)
     }
+}
+db()
+// app
+const express = require('express')
+const app = express();
+app.get('/', (req, res) => {
+    res.send('hi buddy')
+})
+// db
 
+// add portfolio
+app.get('/portfolio/add', (req, res) => {
 
 })
-app.listen(2030, () => {
-    console.log('server is running at port 2030 http://localhost:2030')
+app.listen(PORT, () => {
+    console.log("http://localhost:" + PORT)
 })
